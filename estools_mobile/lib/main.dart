fda38100fdcd6217cd09b0176034c96b719ed205
chechna9 <ja_bouchouareb@esi.dz>
2022-10-19 14:02:43 +0100

    aded default font

---
+++ estools_mobile/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        alignment: Alignment.center,
+        child: const Text(
+          "Azul World",
+        ),
+      ),
+    );
+  }
+}
