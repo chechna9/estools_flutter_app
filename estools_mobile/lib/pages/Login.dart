0e4fb08c39f1d39c2f9c3f55ecdeff0932b4213d
chechna9 <ja_bouchouareb@esi.dz>
2022-11-20 00:42:42 +0100

    repaire cache

--- estools_mobile/lib/pages/Login.dart
+++ estools_mobile/lib/pages/Login.dart
@@ -1,130 +1,176 @@
-import 'package:flutter/material.dart';
+import 'package:estools_mobile/components/inputField.dart';
 import 'package:estools_mobile/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          color: myWhite,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SvgPicture.asset(
+                "assets/images/svgs/logoBO.svg",
+              ),
+              const LoginForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        color: myWhite,
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(35),
-                child: Image.asset(
-                  "assets/images/logoBO.png",
-                  height: 127,
-                  width: 187,
-                )),
-            SizedBox(height: 35),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 49,
-                  fontFamily: 'Poppins-Bold',
-                  color: myRed,
-                  fontWeight: FontWeight.bold,
+    return Column(
+      children: [
+        Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 40,
+            color: myRed,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomInputField(
+                labelText: 'Email',
+              ),
+              const SizedBox(height: 15),
+              const CustomPasswordInput(
+                labelText: 'Password',
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, registerRoute);
+                },
+                child: const Text(
+                  "don't  have an account ?",
+                  style: TextStyle(color: Colors.black),
                 ),
               ),
-            ),
-            Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  height: 59,
-                  width: 320,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: myGrey,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        labelText: 'Email',
-                        hintText: 'Please use appropriate email form'),
-                  ),
-                )),
-            Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  width: 320,
-                  height: 59,
-                  child: TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: myGrey,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                        labelText: 'Password',
-                        hintText: 'Please use secure password'),
-                  ),
-                )),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "Don't have an account?",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "Forgot your password?",
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                SizedBox(
-                  height: 59,
-                  width: 160,
-                  child: ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        primary: myGrey,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                      ),
-                      onPressed: () {},
-                      icon: ImageIcon(
-                          AssetImage('assets/images/logo_googleg_48dp.png')),
-                      label: Text(
-                        'Sign in with Google',
-                        style: TextStyle(
-                          fontSize: 9,
-                          color: Colors.black,
-                        ),
-                      )),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "forget your password ?",
+                  style: TextStyle(color: Colors.black),
                 ),
-                SizedBox(
-                  height: 59,
-                  width: 160,
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: myDark,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                      ),
-                      onPressed: () {},
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          color: myWhite,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 27,
-                        ),
-                      )),
-                )
-              ],
-            )
-          ],
+              ),
+              Row(
+                children: const [
+                  Flexible(child: SIGoogle_Button()),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Flexible(child: Login_Button()),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Login_Button extends StatelessWidget {
+  const Login_Button({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18,
+          vertical: 20,
+        ),
+        backgroundColor: myDark,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
         ),
       ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Login',
+            style: TextStyle(
+              fontSize: 25,
+              color: myWhite,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+      onPressed: () {},
+    );
+  }
+}
+
+class SIGoogle_Button extends StatelessWidget {
+  const SIGoogle_Button({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+        backgroundColor: myGrey,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          Image.asset(
+            'assets/images/pngs/logo_googleg_48dp.png',
+            width: 32,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          const Expanded(
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 10,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+      onPressed: () {},
     );
   }
 }
